8d25503657b310fdd0d843cecd227a218c783a65
0Johny0 <45623086+0Johny0@users.noreply.github.com>
2024-03-21 01:13:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git pswldep https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+echo 'src-git quickstart https://github.com/iStore-Snapshot/istore-quickstart' >>feeds.conf.default
